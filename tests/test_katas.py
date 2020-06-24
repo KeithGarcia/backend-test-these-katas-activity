a2abf494d945df2a22e4db0d9c1008eaaa4b3424
Keith Garcia <Keith.Garcia444@gmail.com>
2020-06-24 17:39:22 -0400

    final for grading

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -4,19 +4,25 @@ import katas
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        self.assertEqual(katas.add(10, 5), 15)
+        self.assertEqual(katas.add(-1, 1), 0)
+        self.assertEqual(katas.add(-1, -1), -2)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        self.assertEqual(katas.multiply(10, 5), 50)
+        self.assertEqual(katas.multiply(-1, 1), -1)
+        self.assertEqual(katas.multiply(-1, -1), 1)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        self.assertEqual(katas.power(2, 3), 8)
+        self.assertEqual(katas.power(-4, 3), -64)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        self.assertEqual(katas.factorial(4), 24)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        self.assertEqual(katas.fibonacci(1), 0)
+        self.assertEqual(katas.fibonacci(8), 13)
 
 
 if __name__ == '__main__':
